8a2678e5c2e0bf59c0ec6fa54a11e3eefd860f11
FrancisClerfayt <57665204+FrancisClerfayt@users.noreply.github.com>
2020-08-08 15:27:44 +0200

    Initialisation du repertoire

---
+++ inc/header.php
@@ -0,0 +1,3 @@
+<header>
+  <img src="img/logos_nf_logo-30.png" alt="Logo de la Nouvelle Forge.">
+</header>
---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="fonts/fonts.css">
+    <title>Nouvelle Forge</title>
+  </head>
+  <body>
+    <?php include "inc/header.php" ?>
+
+    <main>
+      
+    </main>
+
+    <?php // include "inc/footer.php" ?>
+  </body>
+</html>

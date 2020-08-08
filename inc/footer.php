dbb1989c67ca3693b91626178e4be9f58d84d0d7
FrancisClerfayt <57665204+FrancisClerfayt@users.noreply.github.com>
2020-08-08 21:24:28 +0200

    intégration modif au format php + renommage fichier img

---
+++ inc/footer.php
@@ -0,0 +1,8 @@
+<footer>
+  <p> Notre Newsletter </p>
+  <ul class=footerD>
+    <li>Conditions Générales</li>
+    <li>Mentions Légales</li>
+    <li>FAQ</li>
+  </ul>
+</footer>
--- index.php
+++ index.php
@@ -1,19 +1,82 @@
 <!DOCTYPE html>
 <html lang="fr" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/style.css">
-    <link rel="stylesheet" href="fonts/fonts.css">
-    <title>Nouvelle Forge</title>
-  </head>
-  <body>
-    <?php include "inc/header.php" ?>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="icon" type="image/png" href="img/favicon.png" />
+  <link rel="stylesheet" href="css/style.css">
+  <link rel="stylesheet" href="fonts/fonts.css">
+  <title>Nouvelle Forge</title>
+</head>
+<body>
+  <?php include "inc/header.php" ?>
 
-    <main>
-      
-    </main>
+  <main>
+    <h3> Nouvelle forge : </h3>
+    <img class="imageactu" src="images/actu1.png" alt="imageactu">
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+      Fusce nulla augue, luctus eu ultrices tristique, luctus ut dolor.
+      Ut interdum nisi nec malesuada commodo.
+      Maecenas at lacus molestie mauris vehicula consectetur sit amet non nisl.
+      Pellentesque porttitor ante in tellus pellentesque, a finibus turpis convallis.
+      Nulla porta nisi sapien, id egestas mi dictum sit amet. In hac habitasse platea dictumst.
+      Vivamus in mi nec eros iaculis blandit. Mauris vehicula vel nibh ut suscipit.
+      Sed dictum augue sed placerat tincidunt. Aliquam et efficitur quam.
+      Pellentesque tellus sem, molestie vel felis id, maximus lacinia lacus.
+      Pellentesque cursus ex et ultricies ultrices.
+      Nullam lobortis malesuada felis vitae placerat.
+      Integer non suscipit felis. Suspendisse vulputate at odio id ornare.
+    </p>
 
-    <?php // include "inc/footer.php" ?>
-  </body>
+    <h3> Titre Actualité 1</h3>
+    <img class="imageactu" src="images/actu1.png" alt="imageactu">
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+      Fusce nulla augue, luctus eu ultrices tristique, luctus ut dolor.
+      Ut interdum nisi nec malesuada commodo.
+      Maecenas at lacus molestie mauris vehicula consectetur sit amet non nisl.
+      Pellentesque porttitor ante in tellus pellentesque, a finibus turpis convallis.
+      Nulla porta nisi sapien, id egestas mi dictum sit amet. In hac habitasse platea dictumst.
+      Vivamus in mi nec eros iaculis blandit. Mauris vehicula vel nibh ut suscipit.
+      Sed dictum augue sed placerat tincidunt. Aliquam et efficitur quam.
+      Pellentesque tellus sem, molestie vel felis id, maximus lacinia lacus.
+      Pellentesque cursus ex et ultricies ultrices.
+      Nullam lobortis malesuada felis vitae placerat.
+      Integer non suscipit felis. Suspendisse vulputate at odio id ornare.
+    </p>
+
+    <h3> Titre Actualité 2</h3>
+    <img class="imageactu" src="images/actu1.png" alt="imageactu">
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+      Fusce nulla augue, luctus eu ultrices tristique, luctus ut dolor.
+      Ut interdum nisi nec malesuada commodo.
+      Maecenas at lacus molestie mauris vehicula consectetur sit amet non nisl.
+      Pellentesque porttitor ante in tellus pellentesque, a finibus turpis convallis.
+      Nulla porta nisi sapien, id egestas mi dictum sit amet. In hac habitasse platea dictumst.
+      Vivamus in mi nec eros iaculis blandit. Mauris vehicula vel nibh ut suscipit.
+      Sed dictum augue sed placerat tincidunt. Aliquam et efficitur quam.
+      Pellentesque tellus sem, molestie vel felis id, maximus lacinia lacus.
+      Pellentesque cursus ex et ultricies ultrices.
+      Nullam lobortis malesuada felis vitae placerat.
+      Integer non suscipit felis. Suspendisse vulputate at odio id ornare.
+    </p>
+
+    <div id="partenaires">
+      <a href="https://www.valenciennes.fr">
+        <img class="logo_partenaire" src="img/valenciennelogo.png" alt="valenciennes" />
+      </a>
+      <a href="https://www.hautsdefrance.fr">
+        <img class="logo_partenaire" src="img\haut_de_france_logo.png" alt="haut_de_frace" />
+      </a>
+      <a href="https://europe-en-hautsdefrance.eu">
+        <img class="logo_partenaire" src="img\europe_logo.png" alt="europe" />
+      </a>
+    </div>
+
+  </main>
+
+  <?php include "inc/footer.php" ?>
+</body>
 </html>

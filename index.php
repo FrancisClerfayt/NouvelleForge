3c7d89e932de103a08079793bbc986979c2cd9f6
Francis Clerfayt <francis.clerfayt@gmail.com>
2020-08-19 14:23:29 +0200

    modif en php assimilatoin des modif ajout page pepiniere

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-VA-Compatible" content="ie=edge">
   <link rel="icon" type="image/png" href="img/favicon.png" />
   <link rel="stylesheet" href="css/style.css">
   <link rel="stylesheet" href="fonts/fonts.css">

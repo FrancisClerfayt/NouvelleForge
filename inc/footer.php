6a8c6d11c524590a11c661e7e1f28c18d5e06651
Francis Clerfayt <francis.clerfayt@gmail.com>
2020-08-25 11:39:27 +0200

    Inclusion de la page formations + test sur modif du footer

--- inc/footer.php
+++ inc/footer.php
@@ -1,14 +1,3 @@
-<div id="partenaires">
-  <a href="https://www.valenciennes.fr">
-    <img class="logo_partenaire" src="img/valenciennelogo.png" alt="valenciennes">
-  </a>
-  <a href="https://www.hautsdefrance.fr">
-    <img class="logo_partenaire" src="img\haut_de_france_logo.png" alt="haut_de_frace">
-  </a>
-  <a href="https://europe-en-hautsdefrance.eu">
-    <img class="logo_partenaire" src="img\europe_logo.png" alt="europe">
-  </a>
-</div>
 <footer>
   <div class="newsletter">
     <div class="">
@@ -23,6 +12,17 @@
     <a href="#">Conditions Générales</a>
     <a href="#">Mentions Légales</a>
     <a href="#">FAQ</a>
+  </div>
+  <div id="partenaires">
+    <a href="https://www.valenciennes.fr">
+      <img class="logo_partenaire" src="img/valenciennelogo.png" alt="valenciennes">
+    </a>
+    <a href="https://www.hautsdefrance.fr">
+      <img class="logo_partenaire" src="img\haut_de_france_logo.png" alt="haut_de_frace">
+    </a>
+    <a href="https://europe-en-hautsdefrance.eu">
+      <img class="logo_partenaire" src="img\europe_logo.png" alt="europe">
+    </a>
   </div>
 </footer>
 <script type="text/javascript" src="js/script.js"></script>
